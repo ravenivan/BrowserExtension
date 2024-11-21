84db985b797672f7a1f195edd80103d2cc805106
ravenivan <127802097+ravenivan@users.noreply.github.com>
2024-11-20 22:09:18 -0500

    part c in progress

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let myLeads = []
+let myLeads = localStorage.getItem("leadsFromLocalStorage") ? 
+(JSON.parse(localStorage.getItem("leadsFromLocalStorage")), renderLeads()) : 
+[]
 let listItems = ""
 let ulEl = document.getElementById('ul-el')
 let inputEl = document.getElementById('input-el')
 let button = document.getElementById('input-btn')
 
+console.log(myLeads);
+
+
 button.addEventListener('click', () => {
   myLeads.push(inputEl.value)
   renderLeads()
+  localStorage.setItem("leadsFromLocalStorage", JSON.stringify(myLeads) )
+  console.log( localStorage.getItem("leadsFromLocalStorage") )
 })
 
 const renderLeads = () => {
@@ -19,3 +26,7 @@ const renderLeads = () => {
     ulEl.innerHTML = listItems
   }
 }
+
+
+
+
