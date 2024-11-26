b6489a39cf3210da467deb8f5ffa0f2c044ccc8a
ravenivan <127802097+ravenivan@users.noreply.github.com>
2024-11-25 20:12:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-let myLeads = localStorage.getItem("leadsFromLocalStorage") ? 
-(JSON.parse(localStorage.getItem("leadsFromLocalStorage")), renderLeads()) : 
-[]
+let myLeads = localStorage.getItem("leadsFromLocalStorage") 
+? JSON.parse(localStorage.getItem("leadsFromLocalStorage")) 
+: []
 let listItems = ""
 let ulEl = document.getElementById('ul-el')
 let inputEl = document.getElementById('input-el')
 let button = document.getElementById('input-btn')
-
-console.log(myLeads);
-
-
-button.addEventListener('click', () => {
-  myLeads.push(inputEl.value)
-  renderLeads()
-  localStorage.setItem("leadsFromLocalStorage", JSON.stringify(myLeads) )
-  console.log( localStorage.getItem("leadsFromLocalStorage") )
-})
+let saveButton = document.getElementById('save-btn')
+let deleteButton = document.getElementById('delete-btn')
 
 const renderLeads = () => {
+  if (myLeads.length === 0) {
+    ulEl.innerHTML = ""
+    return
+  }
+
   listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
     listItems += 
@@ -26,6 +23,38 @@ const renderLeads = () => {
     ulEl.innerHTML = listItems
   }
 }
+
+if (myLeads.length > 0) {
+  renderLeads()
+}
+
+
+console.log(myLeads);
+
+button.addEventListener('click', () => {
+  myLeads.push(inputEl.value)
+  renderLeads()
+  localStorage.setItem("leadsFromLocalStorage", JSON.stringify(myLeads) )
+  console.log( localStorage.getItem("leadsFromLocalStorage") )
+})
+
+saveButton.addEventListener('click', () => {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function 
+    (tabs) {
+      myLeads.push(tabs[0].url)
+      localStorage.setItem("leadsFromLocalStorage", JSON.stringify(myLeads))
+      renderLeads()
+    });
+})
+
+deleteButton.addEventListener('click', () => {
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+  console.log('asd');
+  
+})
+
 
 
 
